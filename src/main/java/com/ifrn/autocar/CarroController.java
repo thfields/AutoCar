c688f1e526126c40fbe7bc28d69c2afed640553c
Thiago Campos Batista <20231148060031@ifrn.local>
2025-05-09 14:30:12 -0300

    feat: finalizando crud de carros

--- src/main/java/com/ifrn/autocar/CarroController.java
+++ src/main/java/com/ifrn/autocar/CarroController.java
@@ -1,7 +1,12 @@
 package com.ifrn.autocar;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class CarroController {
@@ -9,15 +14,46 @@ public class CarroController {
     @Autowired
     CarroRepository carroRepository;
 
-    @GetMapping("/carro")
-    public String hello(){
-        Carro carro = carroRepository.findById(1);
+    @GetMapping("/carro/{id}")
+    public ResponseEntity<Carro> getById(@PathVariable int id) {
+        Optional<Carro> carroOptional = carroRepository.findById(id);
+        if (carroOptional.isPresent()) {
+            return ResponseEntity.ok(carroOptional.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
-        return carro.getModelo();
+    @GetMapping("/carros")
+    public List<Carro> getAll() {
+        return carroRepository.findAll();
     }
 
     @PostMapping("/salvar")
-    public void salvarCarro(@RequestBody Carro carro){
-        carroRepository.save(carro);
+    @ResponseStatus(HttpStatus.CREATED)
+    public Carro salvarCarro(@RequestBody Carro carro) {
+        return carroRepository.save(carro);
+    }
+
+    @DeleteMapping("/carro/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarCarro(@PathVariable int id) {
+        carroRepository.deleteById(id);
+    }
+
+    @PutMapping("/carro/{id}")
+    public ResponseEntity<Carro> atualizarCarro(@PathVariable int id, @RequestBody Carro carroAtualizado) {
+        Optional<Carro> carroOptional = carroRepository.findById(id);
+        if (carroOptional.isPresent()) {
+            Carro carroExistente = carroOptional.get();
+            carroExistente.setModelo(carroAtualizado.getModelo());
+            carroExistente.setMarca(carroAtualizado.getMarca());
+            carroExistente.setAno(carroAtualizado.getAno());
+
+            Carro carroSalvo = carroRepository.save(carroExistente);
+            return ResponseEntity.ok(carroSalvo);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
--- src/main/java/com/ifrn/autocar/CarroRepository.java
+++ src/main/java/com/ifrn/autocar/CarroRepository.java
@@ -1,26 +1,9 @@
 package com.ifrn.autocar;
 
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.PersistenceContext;
-import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PostMapping;
-
-import java.util.List;
 
 @Repository
-public class CarroRepository {
+public interface CarroRepository extends JpaRepository<Carro, Integer> {
 
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    @Transactional
-    public Carro findById(int id) {
-        return entityManager.find(Carro.class, id);
-    }
-
-    @Transactional
-    public void save(Carro carro) {
-        entityManager.persist(carro);
-    }
 }
